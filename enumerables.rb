bbb62f7d4a175dff6cab4a3f97ec7330513e5062
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-07-13 23:30:07 +0300

    Add my_each method to enumerables module.

---
+++ enumerables.rb
@@ -0,0 +1,25 @@
+module Enumerables
+  def my_each
+    return enum_for unless block_given?
+    array = is_a?(Range) ? to_a : self
+    array.length.times { |index| yield(array[index]) }
+    array
+  end
+
+    def my_each_with_index
+    end
+
+  def my_select; end
+
+  def my_all; end
+
+  def my_any; end
+
+  def my_none; end
+
+  def my_count; end
+
+  def my_map; end
+
+  def my_inject; end
+end
