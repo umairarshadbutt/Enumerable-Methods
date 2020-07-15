fa8dda41abbd8e7209386b316ba45b5300b62004
umairarshadbutt <umair6622@gmail.com>
2020-07-15 13:10:41 +0500

    Implements required TSE changes

--- main.rb
+++ main.rb
@@ -84,7 +84,6 @@ module Enumerable
   end
 
   def my_inject(number = nil, sym = nil)
-    raise LocalJumpError unless block_given? || !number.empty?
 
     if block_given?
       accumulator = number
