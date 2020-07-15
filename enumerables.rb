7ec8ae714b273aa3b40f2866d88b5c892b9e6f17
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-07-15 20:10:16 +0300

    Fix my_none.

--- enumerables.rb
+++ enumerables.rb
@@ -61,7 +61,21 @@ module Enumerable
   end
 
   def my_none?(*args)
-    my_any?(*args)
+    return_value = true
+    if block_given?
+      self.my_each do |el|
+        return_value = false if yield(el) == true
+      end
+    elsif args[0].is_a? Class
+      self.my_each { |index| return_value = true unless index.class.ancestors.include?(args[0]) }
+    elsif args[0].is_a? Regexp
+      self.my_each { |index| return_value = false unless args[0].match(index) }
+    elsif args.empty?
+      return include?(nil) || include?(true) ? return_value = true : return_value = false
+    else
+      self.my_each { |index| return_value = true unless index == args[0] }
+    end
+    return_value
   end
 
   def my_count(arg = nil)
@@ -113,3 +127,29 @@ end
 def multiply_els(array)
   array.my_inject(:*)
 end
+
+
+# 6. my_none? (example test cases)
+puts "My None Method"
+p [3, 5, 7, 11].my_none? { |n| n.even? } # => true
+p ["sushi", "pizza", "burrito"].my_none? { |word| word[0] == "a" } # => true
+p [3, 5, 4, 7, 11].my_none? { |n| n.even? } # => false (Not working)
+p ["asparagus", "sushi", "pizza", "apple", "burrito"].my_none? { |word| word[0] == "a" } # => false (Not working)
+# test cases required by tse reviewer
+p [nil, false, nil, false].my_none? # => true
+p [1, 2 ,3].my_none? # => false
+p [1, 2 ,3].my_none?(String) # => true
+p [1,2,3,4,5].my_none?(2) # => false
+p [1, 2, 3].my_none?(4) # => true
+
+puts "Origina None"
+p [3, 5, 7, 11].none? { |n| n.even? } # => true
+p ["sushi", "pizza", "burrito"].none? { |word| word[0] == "a" } # => true
+p [3, 5, 4, 7, 11].none? { |n| n.even? } # => false (Not working)
+p ["asparagus", "sushi", "pizza", "apple", "burrito"].none? { |word| word[0] == "a" } # => false (Not working)
+# test cases required by tse reviewer
+p [nil, false, nil, false].none? # => true
+p [1, 2 ,3].none? # => false
+p [1, 2 ,3].none?(String) # => true
+p [1,2,3,4,5].none?(2) # => false
+p [1, 2, 3].none?(4) # => true
