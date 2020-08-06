3e99b0fb24fc2837aa7071ded277d1235923b1a8
umairarshadbutt <umair6622@gmail.com>
2020-08-06 19:30:20 +0500

    fix rubocop issues

--- spec/test.rb
+++ spec/test.rb
@@ -1,8 +1,8 @@
 require_relative '../enumerables.rb'
 describe Enumerable do
-describe '#my_each' do
-it 'return a array' do
-expect([1,2,3,4].my_each).to eq([1,2,3,4])
+  describe '#my_each' do
+    it 'return a array' do
+      expect([1, 2, 3, 4].my_each).to eq([1, 2, 3, 4])
+    end
+  end
 end
-end
-end 
